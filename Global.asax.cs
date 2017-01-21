1fc21fc18aec30c05ffd3d1b54c7811772d50c8e
bschubarg <bschubarg@hotmail.com>
2017-01-20 20:09:36 -0500

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,42 @@
+﻿// Lotto Numbers Service and Application.
+// Open source.  This is a project to demonstrate 
+// the various technologies to use when gathering data
+// and publishing data by various means.  I created
+// this project only for my personal use.  Any alterations
+// by others is welcomed.
+// 
+// I do not pretend to be an expert on these technologies
+// but rather a demonstration of my approach to satisfy
+// certain requirements.
+// 
+// Acknowledgments: https://github.com/rubicon-oss/LicenseHeaderManager/wiki - License Header Snippet
+//					https://www.codeproject.com/Articles/1041115/Webscraping-with-Csharp - ScrapySharp
+// 
+// Copyright (c) 2016 William Schubarg
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace LottoWebService
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
