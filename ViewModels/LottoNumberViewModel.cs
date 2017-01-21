6ec342b2135da6a2127f970f71e2b464f1bd3570
bschubarg <bschubarg@hotmail.com>
2017-01-20 20:08:59 -0500

    Add files via upload

---
+++ ViewModels/LottoNumberViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LottoCommon;
+
+namespace LottoWebService.ViewModels
+{
+    [Serializable]
+    public class LottoEntityViewModel : LottoEntity
+    {
+    }
+    [Serializable]
+    public class LottoNumberViewModel : LottoNumber
+    {
+    }
+    [Serializable]
+    public class LottoNumbersListViewModel : LottoNumbers
+    {
+        public LottoNumbersListViewModel(LottoEntity entity) : base(entity)
+        {
+        }
+    }
+}
